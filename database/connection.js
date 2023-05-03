ab7afb9fa3e07825e4b5ce5ce066e123ab8b4a57
Gustavo Soares <gustavohvs.dev@gmail.com>
2023-05-03 10:57:05 -0300

    projeto

---
+++ database/connection.js
@@ -0,0 +1,9 @@
+var knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'dbnodejs'
+    }
+})
